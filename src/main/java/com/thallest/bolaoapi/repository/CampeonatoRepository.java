0cae3e6e1de03c7c0a5253faaea7794b6394a051
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 20:58:41 -0300

    chore: add src/main/java/com/thallest/bolaoapi/repository/CampeonatoRepository.java

---
+++ src/main/java/com/thallest/bolaoapi/repository/CampeonatoRepository.java
@@ -0,0 +1,8 @@
+package com.thallest.bolaoapi.repository;
+
+import com.thallest.bolaoapi.domain.Campeonato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampeonatoRepository extends JpaRepository<Campeonato, Long> {
+}
+
