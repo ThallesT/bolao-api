c3303e6b5d3e847d955b3e15ad3f8d1d32982a30
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 20:59:24 -0300

    chore: add src/main/java/com/thallest/bolaoapi/repository/PartidaRepository.java

---
+++ src/main/java/com/thallest/bolaoapi/repository/PartidaRepository.java
@@ -0,0 +1,8 @@
+package com.thallest.bolaoapi.repository;
+
+import com.thallest.bolaoapi.domain.Partida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartidaRepository extends JpaRepository<Partida, Long> {
+}
+
