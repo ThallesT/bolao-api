6b2f888866e676924c2edcdd4bb0d10d72013458
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 20:30:51 -0300

    chore: add src/main/java/com/thallest/bolaoapi/BolaoApiApplication.java

---
+++ src/main/java/com/thallest/bolaoapi/BolaoApiApplication.java
@@ -0,0 +1,14 @@
+package com.thallest.bolaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApiApplication.class, args);
+	}
+
+}
+
