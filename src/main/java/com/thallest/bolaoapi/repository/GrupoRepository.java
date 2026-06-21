105d9b30c4a680c748ddae97fa4a5c9935a4ccfd
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:37:42 -0300

    chore: sync GrupoRepository.java

--- src/main/java/com/thallest/bolaoapi/repository/GrupoRepository.java
+++ src/main/java/com/thallest/bolaoapi/repository/GrupoRepository.java
@@ -1,12 +1,13 @@
 package com.thallest.bolaoapi.repository;
 
 import com.thallest.bolaoapi.domain.Grupo;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+public interface GrupoRepository extends JpaRepository<Grupo, UUID> {
 
     boolean existsByAccessCodeIgnoreCase(String accessCode);
 
-    boolean existsByAccessCodeIgnoreCaseAndIdNot(String accessCode, Long id);
+    boolean existsByAccessCodeIgnoreCaseAndIdNot(String accessCode, UUID id);
 }
 
