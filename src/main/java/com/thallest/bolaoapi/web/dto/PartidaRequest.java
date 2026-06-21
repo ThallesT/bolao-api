77dfcc072b9bfdf2da41e16b977480bc408c2b5f
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:43:35 -0300

    chore: sync PartidaRequest.java

--- src/main/java/com/thallest/bolaoapi/web/dto/PartidaRequest.java
+++ src/main/java/com/thallest/bolaoapi/web/dto/PartidaRequest.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record PartidaRequest(
-    @NotNull Long championshipId,
+    @NotNull UUID championshipId,
     @NotBlank @Size(max = 100) String homeTeam,
     @NotBlank @Size(max = 100) String awayTeam,
     @NotNull LocalDateTime matchDate,
