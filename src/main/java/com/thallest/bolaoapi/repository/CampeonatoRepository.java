a5c56b5462d73ef07df02b6a79b927c00139765b
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:37:24 -0300

    chore: sync CampeonatoRepository.java

--- src/main/java/com/thallest/bolaoapi/repository/CampeonatoRepository.java
+++ src/main/java/com/thallest/bolaoapi/repository/CampeonatoRepository.java
@@ -1,8 +1,9 @@
 package com.thallest.bolaoapi.repository;
 
 import com.thallest.bolaoapi.domain.Campeonato;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CampeonatoRepository extends JpaRepository<Campeonato, Long> {
+public interface CampeonatoRepository extends JpaRepository<Campeonato, UUID> {
 }
 
