85b62888ea263852d31c71bf89d70188bf918983
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:58:15 -0300

    feat: add UnauthorizedException.java

---
+++ src/main/java/com/thallest/bolaoapi/web/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.thallest.bolaoapi.web.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
