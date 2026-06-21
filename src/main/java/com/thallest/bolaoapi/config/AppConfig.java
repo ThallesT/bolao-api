7f03d537b419def55fddf0d5e5094e6e22bb8420
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:54:40 -0300

    feat: add AppConfig.java

---
+++ src/main/java/com/thallest/bolaoapi/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.thallest.bolaoapi.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@EnableConfigurationProperties(AuthProperties.class)
+public class AppConfig {
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
+    }
+}
+
