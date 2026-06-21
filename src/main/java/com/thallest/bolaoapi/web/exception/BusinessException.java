65b032be97c34c71382e2332b63c8b31b40f3331
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:05:23 -0300

    chore: add src/main/java/com/thallest/bolaoapi/web/exception/BusinessException.java

---
+++ src/main/java/com/thallest/bolaoapi/web/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.thallest.bolaoapi.web.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
+
