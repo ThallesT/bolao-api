901c4f910b77f68d7dfa4957ed8d0b46b917ee4f
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:23:24 -0300

    feat: sync PalpiteRepository.java

--- src/main/java/com/thallest/bolaoapi/repository/PalpiteRepository.java
+++ src/main/java/com/thallest/bolaoapi/repository/PalpiteRepository.java
@@ -1,6 +1,7 @@
 package com.thallest.bolaoapi.repository;
 
 import com.thallest.bolaoapi.domain.Palpite;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PalpiteRepository extends JpaRepository<Palpite, Long> {
@@ -8,5 +9,7 @@ public interface PalpiteRepository extends JpaRepository<Palpite, Long> {
     boolean existsByGroupIdAndMatchIdAndUserId(Long groupId, Long matchId, Long userId);
 
     boolean existsByGroupIdAndMatchIdAndUserIdAndIdNot(Long groupId, Long matchId, Long userId, Long id);
+
+    List<Palpite> findByGroupId(Long groupId);
 }
 
