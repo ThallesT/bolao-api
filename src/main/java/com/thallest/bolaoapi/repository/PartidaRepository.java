a04b1112eced88b150897a3f6db806e88b9206fb
Thalles Tavares <17638106+ThallesT@users.noreply.github.com>
2026-06-20 21:38:18 -0300

    chore: sync PartidaRepository.java

--- src/main/java/com/thallest/bolaoapi/repository/PartidaRepository.java
+++ src/main/java/com/thallest/bolaoapi/repository/PartidaRepository.java
@@ -1,8 +1,9 @@
 package com.thallest.bolaoapi.repository;
 
 import com.thallest.bolaoapi.domain.Partida;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PartidaRepository extends JpaRepository<Partida, Long> {
+public interface PartidaRepository extends JpaRepository<Partida, UUID> {
 }
 
